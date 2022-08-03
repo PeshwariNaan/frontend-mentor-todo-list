9e0ce2a648a44011949aec4666302e15b7b96fec
J.D. Browne <PeshwariNaan01@proton.me>
2022-08-03 10:58:02 +0400

    Added theme context for dark and light modes.

--- src/components/background/background.component.jsx
+++ src/components/background/background.component.jsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
 import { BackgroundContainer, BackgroundImage } from './background.styles';
 import BgDarkDesktop from '../../assets/bg-desktop-dark.jpg'
 import BgDarkMobile from '../../assets/bg-mobile-dark.jpg'
 import BgLightDesktop from '../../assets/bg-desktop-light.jpg'
 import BgLightMobile from '../../assets/bg-mobile-light.jpg'
+import { ThemeContext } from '../../contexts/theme.context';
 
 const Background = () => {
+    const theme = useContext(ThemeContext)
+    const darkMode = theme.state.darkMode;
   return  ( 
   <BackgroundContainer>
     <BackgroundImage src={BgDarkDesktop} alt='background image'/>
---
+++ src/contexts/theme.context.jsx
@@ -0,0 +1,30 @@
+import { useReducer, createContext } from 'react';
+
+export const ThemeContext = createContext();
+
+//Defining action types for the reducer
+actiontypes = {
+  LIGHTMODE: 'LIGHTMODE',
+  DARKMODE: 'DARKMODE',
+};
+
+//initial state for the reducer
+const initialState = { darkMode: false };
+
+// This is a simple change of state a reducer is not neccessary but I'll use one for the practice
+const themeReducer = (state, action) => {
+  switch (action.type) {
+    case actiontypes.LIGHTMODE:
+      return { darkMode: false };
+    case actiontypes.DARKMODE:
+      return { darkMode: true };
+    default:
+        return state
+  }
+};
+
+export const ThemeProvider = (props) => {
+    const [state, dispatch] = useReducer(themeReducer, initialState)
+
+    return <ThemeContext.Provider value={{state: state, dispatch: dispatch}}>{children}</ThemeContext.Provider>
+}
